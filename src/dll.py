1e1b7110ca1921457befa50f322b63e2ae0bfee2
Julien Wilson <julienawilson@gmail.com>
2016-12-14 13:44:24 -0800

    dll init, push and append with tests

---
+++ src/dll.py
@@ -0,0 +1,50 @@
+"""Implementation of Doubly Linked List data type."""
+
+
+class DoublyLinkedList(object):
+    """Class representation of doubly linked list."""
+
+    def __init__(self, iterable=None):
+        """Instantiate linked list."""
+        self.head_node = None
+        self.tail_node = None
+        self.length = 0
+        try:
+            for item in iterable:
+                self.push(item)
+        except TypeError:
+            if iterable:
+                print("Please only enter iterable values")
+
+    def push(self, contents):
+        """Add node to this dll."""
+        if self.length == 0:
+            self.head_node = Node(contents, None, None)
+            self.tail_node = self.head_node
+        else:
+            self.head_node = Node(contents, self.head_node, None)
+        self.length += 1
+
+    def append(self, contents):
+        """Add node to this dll."""
+        if self.length == 0:
+            self.tail_node = Node(contents, None, None)
+            self.head_node = self.tail_node
+        else:
+            self.tail_node = Node(contents, None, self.tail_node)
+        self.length += 1
+
+    def pop(self):
+        """Remove and return the current head node."""
+
+
+
+class Node(object):
+    """Class representation of doubly linked list node."""
+
+    def __init__(self, contents, next_node, previous_node):
+        """Instantiate doubly linked list node."""
+        self.contents = contents
+        self.next_node = next_node
+        self.previous_node = previous_node
+
---
+++ src/test_dll.py
@@ -0,0 +1,43 @@
+"""Tests for dll.py."""
+
+
+def test_node_init():
+    """Test node class init."""
+    from dll import Node
+    new_node = Node(0, None, None)
+    assert new_node.contents == 0
+    assert new_node.next_node is None
+    assert new_node.previous_node is None
+
+
+def test_dll_init():
+    """Test for LinkedList init."""
+    from dll import DoublyLinkedList
+    new_dll = DoublyLinkedList()
+    assert new_dll.length == 0
+    assert new_dll.head_node is None
+    assert new_dll.tail_node is None
+
+
+def test_linkedlist_push():
+    """Test for LinkedList push."""
+    from dll import DoublyLinkedList
+    new_dll = DoublyLinkedList()
+    new_dll.push("new")
+    assert new_dll.length == 1
+    assert new_dll.head_node.contents == "new"
+    new_dll.push("second")
+    assert new_dll.length == 2
+    assert new_dll.head_node.contents == "second"
+
+
+def test_linkedlist_append():
+    """Test for LinkedList push."""
+    from dll import DoublyLinkedList
+    new_dll = DoublyLinkedList()
+    new_dll.append("new")
+    assert new_dll.length == 1
+    assert new_dll.tail_node.contents == "new"
+    new_dll.append("second")
+    assert new_dll.length == 2
+    assert new_dll.tail_node.contents == "second"
