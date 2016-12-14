ea3d7594d30d0d69586605cc2f62cc80306b9fbb
jordan <j.schatzman@outlook.com>
2016-12-14 13:04:10 -0800

    change stack to use composition

--- src/stack.py
+++ src/stack.py
@@ -1,40 +1,24 @@
 """Implementation of Stack data type."""
 
+from linked_list import LinkedList
+
 
 class Stack(object):
     """Class representation of a stack."""
 
     def __init__(self, iterable=None):
         """Instantiate stack."""
-        self.head_node = None
-        self.length = 0
-        if iterable:
-            for item in iterable:
-                self.push(item)
+        self.linked_list = LinkedList(iterable)
+        self.length = self.linked_list.length
+        self.head_node = self.linked_list.head_node
 
     def push(self, contents):
         """Add node to this stack."""
-        if self.head_node:
-            self.head_node = Node(contents, self.head_node)
-        else:
-            self.head_node = Node(contents, None)
-        self.length += 1
-
+        self.linked_list.push(contents)
+        self.head_node = self.linked_list.head_node
 
     def pop(self):
         """Remove and return the current head node."""
-        if not self.head_node:
-            raise NameError('Stack is empty, cannot pop!')
-        old_head_node = self.head_node
-        self.head_node = self.head_node.next_node
-        self.length -= 1
+        old_head_node = self.linked_list.pop()
+        self.head_node = self.linked_list.head_node
         return old_head_node
-
-
-class Node(object):
-    """Class representation of linked list node."""
-
-    def __init__(self, contents, next_node):
-        """Instantiate linked list node."""
-        self.contents = contents
-        self.next_node = next_node
--- src/test_stack.py
+++ src/test_stack.py
@@ -1,30 +1,21 @@
 """Tests for stack.py."""
 
 
-def test_node_init():
-    """Test node class init."""
-    from stack import Node
-    new_node = Node(1, None)
-    assert new_node.contents == 1 and new_node.next_node is None
-
-
 def test_stack_init():
     """Test for Stack init."""
-    from stack import Node, Stack
+    from stack import Stack
     new_stack = Stack()
     assert new_stack.length == 0
     assert new_stack.head_node is None
     new_stack2 = Stack([1, 2, 3, 4, 5])
     assert new_stack2.head_node.contents == 5
-    assert new_stack2.length == 5
 
 
 def test_stack_push():
     """Test for Stack push."""
-    from stack import Node, Stack
+    from stack import Stack
     new_stack = Stack()
     new_stack.push(34)
-    assert new_stack.length == 1
     assert new_stack.head_node.contents == 34
     new_stack2 = Stack()
     new_stack2.push(None)
@@ -33,10 +24,18 @@ def test_stack_push():
 
 def test_stack_pop():
     """Test for Stack pop."""
-    from stack import Node, Stack
-    new_node = Node(34, None)
-    new_llist = Stack(new_node)
-    new_llist.push(1)
-    new_llist.pop()
-    assert new_llist.head_node.contents == 34
-    assert new_llist.length == 1
+    from stack import Stack
+    new_stack = Stack(['a', 'b', 'c', 'd'])
+    assert new_stack.length == 4
+    assert new_stack.head_node
+    assert new_stack.pop()
+    assert new_stack.head_node.contents == 'c'
+    empty_stack = Stack()
+    try:
+        empty_stack.pop()
+    except TypeError:
+        assert True
+    one_stack = Stack([1])
+    assert one_stack.head_node.next_node is None
+    one_stack.pop()
+    assert one_stack.head_node is None
