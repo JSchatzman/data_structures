080f0ffd57c09e17da065430dc1e5569bb7aa33a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-19 14:24:28 -0800

    added peek left and size methods to deque

--- src/deque.py
+++ src/deque.py
@@ -6,10 +6,19 @@ from dll import DoublyLinkedList
 class Deque(object):
     """Class implementation of queue.
 
-    1.  Enqueue: Add new head node.
-    2.  Dequeue: Remove tail node.
-    3.  Peek: Display tail node,
-    4.  Size: Display queue length.
+    append(val): adds value to the end of the deque.
+    appendleft(val): adds a value to the front of the deque.
+    pop(): removes a value from the end of the deque and returns it
+            (raises an exception if the deque is empty)
+    popleft(): removes a value from the front of the deque and returns it
+            (raises an exception if the deque is empty)
+    peek(): returns the next value that would be returned by pop but
+            leaves the value in the deque (returns None if the deque is empty)
+    peekleft(): returns the next value that would be returned by popleft
+            but leaves the value in the deque (returns None if the deque
+            is empty)
+    size(): returns the count of items in the queue (returns 0 if the
+            queue is empty)
 
     """
 
@@ -18,43 +27,45 @@ class Deque(object):
         self.dll = DoublyLinkedList(iterable)
         self.head_node = self.dll.head_node
         self.tail_node = self.dll.tail_node
+        self.length = self.dll.length
 
     def append(self, contents):
         """Add value to the end of the deque."""
         self.dll.append(contents)
         self.head_node = self.dll.head_node
         self.tail_node = self.dll.tail_node
+        self.length = self.dll.length
 
     def appendleft(self, contents):
         """Add value to the front of the deque."""
         self.dll.push(contents)
         self.head_node = self.dll.head_node
         self.tail_node = self.dll.tail_node
+        self.length = self.dll.length
 
     def pop(self):
         """Remove and return the current tail node."""
         return self.dll.shift()
         self.head_node = self.dll.head_node
         self.tail_node = self.dll.tail_node
+        self.length = self.dll.length
 
     def popleft(self):
         """Remove and return the current head node."""
         return self.dll.pop()
         self.head_node = self.dll.head_node
         self.tail_node = self.dll.tail_node
+        self.length = self.dll.length
 
     def peek(self):
         """Display but don't remove the contents of tail node."""
         return self.dll.tail_node.contents
-        self.head_node = self.dll.head_node
-        self.tail_node = self.dll.tail_node
 
     def peekleft(self):
-        pass
+        """Display but don't remove the contents of head node."""
+        return self.dll.head_node.contents
 
+    def size(self):
+        """Return the count of items in the queue."""
+        return self.length
 
-test_deq = Deque([1, 2, 3, 4, 5])
-
-test_deq.appendleft(6)
-
-print(test_deq.head_node.contents)
