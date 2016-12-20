fd47ed041f94a8a90afc6004e1c02eee45bb9fc0
jordan <j.schatzman@outlook.com>
2016-12-20 14:39:30 -0800

    fixing init and change swap logic

--- src/heap.py
+++ src/heap.py
@@ -4,10 +4,10 @@
 class Heap(object):
     """Create heap class."""
 
-    def __init(self, iterable=None):
+    def __init__(self, iterable=None):
         """Instantiate a heap."""
         self.heap = [5, 9, 11, 14, 18,
-                     19, 21, 33, 17, 27, 2]
+                     19, 21, 33, 17, 27]
 
     def find_parent_index(self, index):
         """Find parent index of given index."""
@@ -18,12 +18,19 @@ class Heap(object):
         else:
             return index // 2
 
-    def swap_nodes(self, index):
+    def swap_nodes(self, index, parent_index):
         """Swap parent and child nodes if greater."""
         parent_index = self.find_parent_index(index)
-        if self.heap[index] < self.heap[parent_index]:
-            old_parent = self.heap[parent_index]
-            self.heap[parent_index] = self.heap[index]
-            self.heap[index] = old_parent
+        old_parent = self.heap[parent_index]
+        self.heap[parent_index] = self.heap[index]
+        self.heap[index] = old_parent
+        return parent_index
 
-
+    def push(self, contents):
+        """Add value to the end of the heap."""
+        self.heap.append(contents)
+        index = len(self.heap) - 1
+        parent_index = self.find_parent_index(index)
+        while self.heap[index] < self.heap[parent_index]:
+            index = self.swap_nodes(index, parent_index)
+            parent_index = self.find_parent_index(index)
