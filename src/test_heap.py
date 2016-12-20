f7e367383d153918292a5efd70976997904f7f78
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-20 14:40:20 -0800

    created test for parent index function and push function

--- src/test_heap.py
+++ src/test_heap.py
@@ -1,4 +1,33 @@
-"""Test heap.py"""
+"""Test heap.py."""
 
 
-print ('hello')
+import pytest
+
+
+PARENT_CHILD_INDEX = [
+    [9, 4]
+]
+
+
+@pytest.fixture
+def sample_heap():
+    """Create testing heap."""
+    from heap import Heap
+    test_heap = Heap()
+    return test_heap
+
+
+@pytest.mark.parametrize('index, result', PARENT_CHILD_INDEX)
+def test_find_parent_index(index, result):
+    """Test function returns parent index."""
+    from heap import Heap
+    heap = Heap()
+    assert heap.find_parent_index(index) == result
+
+
+def test_push(sample_heap):
+    """Test if push moves item."""
+    # import pdb; pdb.set_trace()
+    size = len(sample_heap.heap)
+    sample_heap.push(6)
+    assert len(sample_heap.heap) == size + 1
