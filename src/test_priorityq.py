0837cb627da29cb3b16fe37e068e45ffde0ed78c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-21 16:04:12 -0800

    fixed insert tests for dictionary format

--- src/test_priorityq.py
+++ src/test_priorityq.py
@@ -27,21 +27,26 @@ def test_multi_inti_priorityq(sample_priorityq):
 
 def test_empty_insert_priorityq(sample_priorityq):
     """Test insert into empty priorityq."""
-    sample_priorityq[0].insert(1)
-    assert len(sample_priorityq[0].data) == 1
+    sample_priorityq[0].insert(2)
+    sample_priorityq[0].insert(1, 3)
+    assert 1 in sample_priorityq[0].data[3]
+    assert 2 in sample_priorityq[0].data[0]
 
 
 def test_one_insert_priorityq(sample_priorityq):
     """Test insert into queue of 1."""
-    sample_priorityq[1].insert(1)
-    print(sample_priorityq[2].data)
-    assert len(sample_priorityq[1].data) == 2
+    sample_priorityq[1].insert(1, 3)
+    sample_priorityq[1].insert(2)
+    assert 1 in sample_priorityq[1].data[3]
+    assert 2 in sample_priorityq[1].data[0]
 
 
 def test_multi_insert_priorityq(sample_priorityq):
     """Test insert into larger queue."""
-    sample_priorityq[2].insert(1)
-    assert 1 in sample_priorityq[2].data[0]
+    sample_priorityq[2].insert(1, 3)
+    sample_priorityq[2].insert(2)
+    assert 2 in sample_priorityq[2].data[0]
+    assert 1 in sample_priorityq[2].data[3]
 
 
 def test_empty_pop_priorityq(sample_priorityq):
