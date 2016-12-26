b0cfa827244457c1d584c356f9c88aa50b55a492
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-26 13:41:21 -0800

    add function to test more examples of add connection and test nodes() displays list of nodes

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -51,3 +51,34 @@ def test_add_edge_on_existing_nodes(sample_graph):
     """Test connections between existing nodes is created."""
     sample_graph[2].add_edge('A', 'B')
     assert sample_graph[2].graph['A'] == ['B']
+
+
+def test_add_edge_when_node2_does_not_exist(sample_graph):
+    """Test connection between new node2 and existing is made."""
+    sample_graph[0].add_edge('A', 'B')
+    assert sample_graph[0].graph == {'A': ['B'], 'B': []}
+
+
+def test_add_edge_when_node1_does_not_exist(sample_graph):
+    """Test connection between new node1 and existing node2."""
+    sample_graph[0].add_edge('B', 'A')
+    assert sample_graph[0].graph == {'A': [], 'B': ['A']}
+
+
+def test_add_edge_when_node1_and_node2_does_not_exist(sample_graph):
+    """Test connection between new node1 and new node2."""
+    sample_graph[1].add_edge('A', 'B')
+    assert sample_graph[1].graph == {'A': ['B'], 'B': []}
+
+
+def test_list_of_nodes_is_displayed(sample_graph):
+    """Test nodes are displayed in a list."""
+    assert sample_graph[0].nodes() == ['A']
+    assert sample_graph[1].nodes() == []
+
+
+def test_list_of_multiple_nodes_is_displayed(sample_graph):
+    """Test nodes in a graph of multiple nodes are returned."""
+    many_nodes = sample_graph[2].nodes()
+    many_nodes.sort()
+    assert many_nodes == ['A', 'B', 'C', 'D']
