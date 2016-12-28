864cf8eb782b8a34e202fa9ed3e73fef0601c841
Julien Wilson <julienawilson@gmail.com>
2016-12-27 16:12:30 -0800

    made tests fixtures

--- src/test_stack.py
+++ src/test_stack.py
@@ -7,84 +7,72 @@ import pytest
 def sample_stack():
     """Create testing stacks."""
     from stack import Stack
-    one_stack = Stack([1])
     empty_stack = Stack()
+    one_stack = Stack([1])
     new_stack = Stack([1, 2, 3, 4, 5])
-    return one_stack, empty_stack, new_stack
+    return (empty_stack, one_stack, new_stack)
 
 
-def test_stack_empty_length():
+def test_stack_empty_length(sample_stack):
     """Test for empty stack length."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert empty_stack.length == 0
+    assert sample_stack[0].length == 0
 
 
-def test_stack_empty_head_node():
+def test_stack_empty_head_node(sample_stack):
     """Test for empty stack head node."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert empty_stack.head_node is None
+    assert sample_stack[0].head_node is None
 
 
-def test_stack_new_length():
+def test_stack_new_length(sample_stack):
     """Test for new stack length."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert new_stack.length == 5
+    assert sample_stack[2].length == 5
 
 
-def test_stack_new_contents():
+def test_stack_new_contents(sample_stack):
     """Test for new stack head node contents."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert new_stack.head_node.contents == 5
+    assert sample_stack[2].head_node.contents == 5
 
 
-def test_stack_new_push():
+def test_stack_new_push(sample_stack):
     """Test for new stack head node contents."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    new_stack.push(34)
-    assert new_stack.head_node.contents == 34
+    sample_stack[2].push(34)
+    assert sample_stack[2].head_node.contents == 34
 
 
-def test_stack_one_push():
+def test_stack_one_push(sample_stack):
     """Test for new stack head node contents."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    one_stack.push(34)
-    assert one_stack.head_node.contents == 34
+    sample_stack[1].push(34)
+    assert sample_stack[1].head_node.contents == 34
 
 
-def test_stack_empty_push():
+def test_stack_empty_push(sample_stack):
     """Test for empty stack head node contents."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    empty_stack.push(34)
-    assert empty_stack.head_node.contents == 34
+    sample_stack[0].push(34)
+    assert sample_stack[0].head_node.contents == 34
 
 
-def test_stack_new_pop():
+def test_stack_new_pop(sample_stack):
     """Test that pop the new stack returns 5."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert new_stack.pop() == 5
+    assert sample_stack[2].pop() == 5
 
 
-def test_stack_new_pop_length():
+def test_stack_new_pop_length(sample_stack):
     """Test that pop the new stack decreases length."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert new_stack.pop() == 5
+    assert sample_stack[2].pop() == 5
 
 
-def test_stack_one_pop():
+def test_stack_one_pop(sample_stack):
     """Test that pop of one stack returns 1."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    assert one_stack.pop() == 1
+    assert sample_stack[1].pop() == 1
 
 
-def test_stack_one_pop_contents():
+def test_stack_one_pop_contents(sample_stack):
     """Test that pop of one stack behaves as expeted."""
-    one_stack, empty_stack, new_stack = sample_stack()
-    one_stack.pop()
-    assert one_stack.head_node is None
+    sample_stack[1].pop()
+    assert sample_stack[1].head_node is None
 
 
-def tst_stack_empty_pop():
+def tst_stack_empty_pop(sample_stack):
     """Test that pop empty stack throws correct error."""
-    one_stack, empty_stack, new_stack = sample_stack()
     with pytest.raises(TypeError):
-        empty_stack.pop()
+        sample_stack[0].pop()
