14dd341abfe8c3d3fbc7640fbd4703ba3cf7a259
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-29 11:53:11 -0800

    added tests for pop for a whole list

--- src/test_heap.py
+++ src/test_heap.py
@@ -11,21 +11,23 @@ HEAP_ORDER_CHECKER = [
     [12, [5, 9, 11, 14, 12, 19, 21, 33, 17, 27, 18]]]
 
 HEAP_POP_CHECKER = [
-    [[5, 9, 11, 14, 18, 19, 21, 33, 17, 27], [9, 14, 11, 17, 18, 19, 21, 33, 27]],
+    [[5, 9, 11, 14, 18, 19, 21, 33, 17, 27],
+     [9, 14, 11, 17, 18, 19, 21, 33, 27]],
     [[1, 4, 5, 6], [4, 6, 5]],
-    [[4], []]]
+    [[1, 2, 3], [2, 3]],
+    [[2, 3], [3]],
+    [[4], []]
+]
 
 
 @pytest.fixture
 def sample_heap():
     """Create testing heap."""
     from heap import Heap
-    test_heap = Heap()
-    test_heap.data = [5, 9, 11, 14, 18,
-                      19, 21, 33, 17, 27]
+    test_heap = Heap([5, 9, 11, 14, 18,
+                      19, 21, 33, 17, 27])
     empty_heap = Heap()
-    one_heap = Heap()
-    one_heap.data = [5]
+    one_heap = Heap(5)
     return empty_heap, one_heap, test_heap
 
 
@@ -40,7 +42,7 @@ def test_various_pushes(value, result):
 @pytest.mark.parametrize('data, result', HEAP_POP_CHECKER)
 def test_various_pops(data, result):
     """Test if various pops result in proper stack."""
-    heap = sample_heap()[2]
+    heap = sample_heap()[0]
     heap.data = data
     heap.pop()
     assert heap.data == result
@@ -70,3 +72,34 @@ def test_pop_on_empty(sample_heap):
     """Test for pop on empty heap."""
     with pytest.raises(IndexError):
         sample_heap[0].pop()
+
+
+def test_pop_returns_a_value(sample_heap):
+    """Test pop returns a value."""
+    val = sample_heap[2].pop()
+    assert val == 5
+
+
+def test_multi_push_and_pop(sample_heap):
+    """Test mutliple push followed by pop follows binheap rules."""
+    sample_heap[0].push(5)
+    sample_heap[0].push(3)
+    sample_heap[0].push(4)
+    sample_heap[0].push(2)
+    sample_heap[0].push(1)
+    sample_heap[0].push(0)
+    sample_heap[0].push(9)
+    sample_heap[0].push(8)
+    sample_heap[0].push(7)
+    sample_heap[0].push(6)
+    print(sample_heap[0].data)
+    sample_heap[0].pop()
+    val = sample_heap[0].pop()
+    assert val == 1
+
+
+def test_pop_in_order_min_to_max(sample_heap):
+    """Test min heap is maintained as pop values."""
+    pop_values = [sample_heap[2].pop() for i in range(1, len(sample_heap[2].data) + 1)]
+    print(pop_values)
+    assert pop_values == [5, 9, 11, 14, 17, 18, 19, 21, 27, 33]
