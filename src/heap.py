7d707a1fb1a32f68a6cf075864799c575ff0ae10
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-29 11:52:47 -0800

    refactored bin heap code to be more DRY

--- src/heap.py
+++ src/heap.py
@@ -17,59 +17,40 @@ class Heap(object):
         except TypeError:
             self.data.append(val)
 
-    def find_parent_index(self, index):
-        """Find parent index of given index."""
-        if index == 0:
-            return None
-        if index % 2 == 0:
-            return index // 2 - 1
-        else:
-            return index // 2
-
-    def swap_nodes(self, index, parent_index):
-        """Swap parent and child nodes if greater."""
-        parent_index = self.find_parent_index(index)
-        old_parent = self.data[parent_index]
-        self.data[parent_index] = self.data[index]
-        self.data[index] = old_parent
-        return parent_index
-
-    def push(self, contents):
+    def push(self, val):
         """Add value to the end of the data."""
-        if len(self.data) < 1:
-            return self.data.append(contents)
-        self.data.append(contents)
-        index = len(self.data) - 1
-        parent_index = self.find_parent_index(index)
-        while (self.data[index] < self.data[parent_index]) and ((index // 2) > 0):
-            index = self.swap_nodes(index, parent_index)
-            parent_index = self.find_parent_index(index)
-        if index in (1, 2) and self.data[index] < self.data[0]:
-            self.swap_nodes(index, 0)
+        self.data.append(val)
+        self._swap_nodes_up(len(self.data) - 1)
 
     def pop(self):
         """Remove head of the heap."""
         if len(self.data) == 0:
             raise IndexError('Cannot pop a empty heap!')
         val = self.data[0]
-        self.data[0] = self.data[len(self.data) - 1]
+        self.data[0] = self.data[-1]
         self.data.pop()
-        index = 0
-        while (index * 2 + 1) < len(self.data):
-            min_child = self.find_min_child(index)
-            if self.data[index] > self.data[min_child]:
-                old_parent = self.data[index]
-                self.data[index] = self.data[min_child]
-                self.data[min_child] = old_parent
-            index = min_child
+        self._swap_nodes_down(0)
         return val
 
-    def find_min_child(self, index):
-        """Find minimum child."""
-        if index * 2 + 1 > len(self.data):
-            return index * 2
-        else:
-            if self.data[index * 2 + 1] < self.data[index * 2 + 2]:
-                return index * 2 + 1
-            else:
-                return index * 2 + 2
+    def _swap_nodes_up(self, idx):
+        """Swap parent and child nodes if greater."""
+        if idx == 0:
+            return 0
+        parent_index = (idx - 1) // 2
+        if self.data[idx] < self.data[parent_index]:
+            self.data[idx], self.data[parent_index] = self.data[parent_index], self.data[idx]
+            self._swap_nodes_up(parent_index)
+
+    def _swap_nodes_down(self, idx):
+        """Swap nodes up until meets minheap."""
+        child1 = (2 * idx) + 1
+        if child1 >= len(self.data):
+            return 0
+        child2 = child1 + 1
+        try:
+            lowest_child = child1 if self.data[child1] < self.data[child2] else child2
+        except IndexError:
+            lowest_child = child1
+        if self.data[idx] > self.data[lowest_child]:
+            self.data[idx], self.data[lowest_child] = self.data[lowest_child], self.data[idx]
+            self._swap_nodes_down(lowest_child)
