19ed83bff4c8e12e58050a366269e68b43e4c90b
jordan <j.schatzman@outlook.com>
2017-01-03 12:07:21 -0800

    creating weighted-graph branch

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name='data_structures',
     description='Implementation of Data Structures.',
     version=0.1,
-    author='Jordan Schatzman, Julien Wilson',
-    author_email='j.schatzman@outlook.com, julienawilson@gmail.com',
+    author='Jordan Schatzman, Julien Wilson, Claire Gatenby',
+    author_email='j.schatzman@outlook.com, julienawilson@gmail.com, clairejgatenby@gmail.com',
     license='MIT',
     package_dir={'': 'src'},
-    py_modules=['linked_list', 'stack', 'dll'],
+    py_modules=['linked_list', 'stack', 'dll', 'simple_graph', 'heap', 'queue', 'deque'],
     extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
 )
