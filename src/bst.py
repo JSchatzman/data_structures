1dd20b1597ce6b61967d88e41dc9acdd77767ddd
jordan <j.schatzman@outlook.com>
2017-01-16 14:46:24 -0800

    finish up depth

--- src/bst.py
+++ src/bst.py
@@ -30,7 +30,7 @@ class BinarySearchTree(object):
         check = self.root
         while check:
             if val < check.contents:
-                print('hello1')
+                #print('hello1')
                 if check.left_child:
                     check = check.left_child
                     continue
@@ -38,7 +38,7 @@ class BinarySearchTree(object):
                 self._size += 1
                 return
             elif val > check.contents:
-                print('hello2')
+                #print('hello2')
                 if check.right_child:
                     check = check.right_child
                     continue
@@ -52,12 +52,37 @@ class BinarySearchTree(object):
         """Return the length of the bst."""
         return self._size
 
+    # def depth(self, root, depth=0, base_root=None):
+    #     # if not base_root:
+    #     #     root
+    #     if root is None:
+    #         return depth
+    #     return max(self.depth(root.left, depth + 1),
+    #                self.depth(root.right, depth + 1))
 
-# bst = BinarySearchTree([40, 20, 50, 30, 15, 60])
-# bst.insert(20)
-# bst.insert(50)
-# bst.insert(30)
-# bst.insert(15)
+    def depth(self, root_check=None, depth_list=None):
+        """Find all depths of this bs
+        t."""
+        if not self.root:
+            return 0
+        if not root_check:
+            root_check = self.root
+        if not depth_list:
+            depth_list = []
+        if not root_check.left_child and not root_check.right_child:
+            return 1
+        if root_check.left_child is None:
+            return self.depth(root_check.right_child) + 1
+        if root_check.right_child is None:
+            return self.depth(root_check.left_child) + 1
+        return max(self.depth(root_check.left_child), self.depth(root_check.right_child)) + 1
 
+#bst = BinarySearchTree([40, 20, 50, 30, 15, 70, 75])
+bst = BinarySearchTree([40, 20, 50, 30, 5, 4])
+
+#bst = BinarySearchTree([40])
+
+
+print(bst.depth())
 
 
