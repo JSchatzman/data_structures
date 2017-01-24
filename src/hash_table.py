80ffe9fea9e37a15c3ae9dcf69b4bfe566c781bb
jordan <j.schatzman@outlook.com>
2017-01-23 16:36:11 -0800

    adding bernstein and control flow

--- src/hash_table.py
+++ src/hash_table.py
@@ -4,27 +4,46 @@
 class HashTable(object):
     """Implementation of a hash table."""
 
-    def __init__(self, size, hash_type=None):
+    def __init__(self, size, hash_type='naive'):
         """Initializes the hash table based on Key Word arg."""
+        if hash_type not in ('bern', 'naive'):
+            raise NameError("Hash type must be 'naive' or 'bern'.")
         self.hash_type = hash_type
         self.size = size
         self.hash_table = [[] for i in range(size)]
 
     def get(self, key):
         """Return the value stored with the given key."""
-        hash_key = self._hash(key) % self.size
-        return self.hash_table[hash_key]
+        hash_key = self.fun_chooser(key) % self.size
+        value = self.hash_table[hash_key]
+        for val, a_key in value:
+            if a_key == key:
+                return val
 
     def set(self, key, val):
         """Store the given val using the given key."""
         if type(key) is not str:
             raise TypeError("Hashes need to be in string format.")
-        hash_key = self._hash(key) % self.size
-        self.hash_table[hash_key].append(val)
+        hash_key = self.fun_chooser(key) % self.size
+        self.hash_table[hash_key].append((val, key))
 
-    def _hash(self, key):
-        """Hash the key provided."""
+    def _naive(self, key):
+        """Hash the key provided naively."""
         hash_key = 0
         for letter in key:
             hash_key += ord(letter)
         return hash_key
+
+    def _bernstein(self, string):
+        """Hash the key using bernstein's method."""
+        h = 3313 # arbitrary prime number
+        for char in string:
+            h = ((h << 5) + h) + ord(char)
+        return h % self.size
+
+    def fun_chooser(self, key):
+        """Control which hash method to use."""
+        if self.hash_type == 'naive':
+            return self._naive(key)
+        else:
+            return self._bernstein(key)
