38c0afad39fc983c8c976d49f1fe7351685330aa
jordan <j.schatzman@outlook.com>
2017-01-25 13:43:12 -0800

    working draft of trie insert

--- src/trie.py
+++ src/trie.py
@@ -1,21 +1,30 @@
 """Implementation of trie."""
 
-class Node(object):
-    """Individual node of tree."""
+# class Node(object):
+#     """Individual node of tree."""
 
-    def __init__(self, value=None):
-        self.value = value
-        self.pointers = ()
+#     def __init__(self, value=None):
+#         self.value = value
+#         self.pointers = ()
 
 class Trie(object):
     """Class representation of trie."""
 
     def __init__(self):
-        self.root = Node()
+        self.root = {}
 
     def insert(self, word):
         """Insert a new word into the trie."""
+        check = self.root
         for letter in word:
-            if letter not in self.root.pointers:
-                pass
-                #add logic here
+            if letter not in check:
+                check.setdefault(letter, {})
+            check = check[letter]
+
+
+
+
+
+
+test = Trie()
+test.insert('kill')
