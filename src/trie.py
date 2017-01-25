e5cddb365d6c00c031e623a24a3da47de6a4cb27
jordan <j.schatzman@outlook.com>
2017-01-25 13:57:50 -0800

    adding size and contains

--- src/trie.py
+++ src/trie.py
@@ -12,17 +12,30 @@ class Trie(object):
 
     def __init__(self):
         self.root = {}
+        self.size = 0
 
-    def insert(self, word):
+    def insert(self, string):
         """Insert a new word into the trie."""
+        if self.contains(string):
+            return
         check = self.root
-        for letter in word:
+        for letter in string:
             if letter not in check:
                 check.setdefault(letter, {})
             check = check[letter]
-
         check['$'] = 'END'
+        self.size += 1
 
+    def contains(self, string):
+        """Return true if the string is in trie else false."""
+        check = self.root
+        for letter in string:
+            if letter not in check:
+                return False
+            check = check[letter]
+        if '$' in check:
+            return True
+        return False
 
 
 
