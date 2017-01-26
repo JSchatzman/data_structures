acbbfd28950f8736ce14a7da8c3eecb65caff9ea
copenbacon <sclary50@gmail.com>
2017-01-25 17:13:20 -0800

    remove method

--- src/trie.py
+++ src/trie.py
@@ -33,7 +33,7 @@ class Trie(object):
             if letter not in check:
                 return False
             check = check[letter]
-        if '$' in check:
+        if '$' in check.keys():
             return True
         return False
 
@@ -46,18 +46,32 @@ class Trie(object):
         if not self.contains(string):
             raise ValueError('The trie does not contain this string.')
         check = self.root
+        list_deleted = []
+        prev = None
         for letter in string:
             check = check[letter]
-        if check.keys().count > 1:
-            del check['END']
+            if prev and len(prev.keys()) > 1:
+                list_deleted.insert(0, letter)
+            prev = check
+        del check['$']
+        # import pdb; pdb.set_trace()
+        if len(check.keys()) > 1:
             return
-        for letter in string.reverse():
-            del check[letter]
-        return
+        list_deleted.insert(0, key)
+        check = self.root
+        prev = None
+        for letter in string:
+            if len(check[letter].keys()) > 1:
+                prev = check[letter]
+            check = check[letter]
+        del prev[list_deleted[0]]
 
-
-
-
-
-test = Trie()
-test.insert('kill')
+from trie import Trie
+t = Trie()
+t.insert("ted")
+t.insert("tea")
+t.insert("teabag")
+t.insert("teabags")
+t.insert("teabagger")
+t.insert("teabaggers")
+t.insert("teabagged")
