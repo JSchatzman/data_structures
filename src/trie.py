fa66f290be27d3e3b542038cbb514a76c57ff342
jordan <j.schatzman@outlook.com>
2017-01-27 13:53:10 -0800

    remove some garbage

--- src/trie.py
+++ src/trie.py
@@ -97,9 +97,3 @@ class Trie(object):
                 for item in self.traversal(newstart):
                     if item != "$":
                         yield item
-trie = Trie()
-trie.insert('alpha')
-trie.insert('alpaca')
-trie.insert('boy')
-gen = trie.traversal('alp')
-print (list(gen))
